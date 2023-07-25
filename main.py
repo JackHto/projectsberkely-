3b4db445438b253b32802c1f090104997250b82e
JackHto <htooismyname51@gmail.com>
2023-07-25 13:49:39 -0700

    main components
    made it so you are able to quit or continue after completing one operative. Made something that would catch any string characters. Also made it possible to be able to take both int and float.

--- main.py
+++ main.py
@@ -1,38 +1,81 @@
-numlist = []
+def cOq():
+    question = input("(C)ontinue or (Q)uit: ")
+    if question.upper() == "C":
+        calculator()
+    elif question.upper() == "Q":
+        exit()
+    else:
+        print("invalid")
+        cOq()
+            
+def calculator():
+    numlist = []
 
-howmany = int(input("How many numbers: "))
+    howmany = input("How many numbers: ")
+    try:
+        howmany = int(howmany)
+    except:
+        print("invalid")
+        calculator()
+    
 
-for x in range(howmany):
-    answer = input("Enter your number: ")
-    numlist.append(answer)
+    for x in range(howmany):
+        answer = input("Enter your number: ")
+        try:
+            answer = float(answer)
+            numlist.append(answer)
+        except:
+            print("invalid")
+            calculator()
+        
+        
+    def addition():
+        x = sum(numlist)
+        print(x)
+        cOq()
     
-    
-    
-def addition():
-    x = sum(numlist)
-    print(x)
- 
-def subtraction():
-    y = float(numlist[0])
-    for x in range(howmany - 1):
-        y -= float(numlist[x+1])
-    print(y)
+    def subtraction():
+        y = float(numlist[0])
+        for x in range(howmany - 1):
+            y -= float(numlist[x+1])
+        print(y)
+        cOq()
 
-def multiply():
-    result = 1
-    for x in numlist:
-        result = result * int(x)
-    print(result)
+    def multiply():
+        result = 1
+        for x in numlist:
+            result = result * int(x)
+        print(result)
+        cOq()
 
-def devide():
-    y = float(numlist[0])
-    for x in range(howmany - 1):
-        y /= float(numlist[x+1])
-    print(y)
-    
-def average():
-    x = float(sum(numlist))/howmany
-    print(x)
-    
-addition()
-    
+    def devide():
+        y = float(numlist[0])
+        for x in range(howmany - 1):
+            y /= float(numlist[x+1])
+        print(y)
+        cOq()
+        
+    def average():
+        x = float(sum(numlist))/howmany
+        print(x)
+        cOq()
+        
+
+    wfun = input("What function, (A)ddition, (S)ubtraction, (M)ultiply, (D)evide, (AV)erage): ")
+
+
+    if wfun.upper() == "A":
+        addition()
+    elif wfun.upper() == "S":
+        subtraction()
+    elif wfun.upper() == "M":
+        multiply()
+    elif wfun.upper() == "D":
+        devide()
+    elif wfun.upper() == "AV":
+        average()
+    else:
+        print("not a valid operation")
+        cOq()
+        
+calculator()
